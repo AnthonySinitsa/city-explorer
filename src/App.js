3b545511e158f3a3112cc89bc58920dc372d0b9c
AnthonySinitsa <itonyemail@gmail.com>
2023-05-15 22:19:12 +0000

    change

--- src/App.js
+++ src/App.js
@@ -46,18 +46,6 @@ class App extends React.Component {
 
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
 
-      </header>
-      <form>
-        <input name='city' />
-        <button type='submit'>get location data</button>
-      </form>
-    </div>
-  );
-}
 
 export default App;
