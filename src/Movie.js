b3f0ea46a16b7fde38ac9447a06a6df5cb35a4d9
AnthonySinitsa <itonyemail@gmail.com>
2023-05-18 21:28:11 +0000

    fixed null images to not render

--- src/Movie.js
+++ src/Movie.js
@@ -14,9 +14,12 @@ class Movie extends React.Component {
               <Card.Text>Overview: {movie.overview}</Card.Text>
               <Card.Text>Average # of Votes: {movie.averageVotes}</Card.Text>
               <Card.Text>Total # of Votes: {movie.totalVotes}</Card.Text>
-              <Card.Img
-                src={`https://image.tmdb.org/t/p/w500${movie.image_url}`}
-              />
+              {movie.image_url
+                ?
+                <Card.Img
+                  src={`https://image.tmdb.org/t/p/w500${movie.image_url}`}
+                /> : ''
+              }
               <Card.Text>Popularity Level: {movie.popularity}</Card.Text>
               <Card.Text>Release Date: {movie.releaseDate}</Card.Text>
               <hr></hr>
